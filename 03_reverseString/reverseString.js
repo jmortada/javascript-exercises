d43eede6542d90a2f1d11f47784420bd0ed5ec68
jmortada <158744391+jmortada@users.noreply.github.com>
2025-02-17 17:31:29 +0200

    tested a reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let res = "";
+    let charsOfString = string.split("");
 
+    for(let i = -1 ; i >= -1 * (charsOfString.length) ; i--){
+        res += charsOfString.at(i);
+    }
+    return res;
 };
 
 // Do not edit below this line
