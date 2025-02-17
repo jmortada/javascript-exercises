d3bae6c7a2c8db430bd1a1b5e300b3ed0ff06574
jmortada <158744391+jmortada@users.noreply.github.com>
2025-02-17 16:50:54 +0200

    function to repeat a string number of times and testing it

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatnum) {
+    if (repeatnum < 0)
+        return "ERROR"
+    let res = "";
+    for (let i = 1 ; i <= repeatnum ; i++){
+        res += string;
+    }
+    return res;
 };
 
 // Do not edit below this line
