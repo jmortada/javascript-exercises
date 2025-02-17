f8ec4f38cc0d1e104a4de38c0a4546ac1379e330
jmortada <158744391+jmortada@users.noreply.github.com>
2025-02-17 22:14:05 +0200

    testing a function that removes elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(arr , ...rmval) {
+    let resArr = [];
+    for (const elem of arr){
+        if (!rmval.includes(elem) ){
+            resArr.push(elem);
+        }
+    }
+    return resArr;
+    
+}
 // Do not edit below this line
 module.exports = removeFromArray;
