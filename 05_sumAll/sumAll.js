6cc053079559342c1b1ea6efd5da9e2341d280d2
jmortada <158744391+jmortada@users.noreply.github.com>
2025-02-18 11:16:36 +0200

    testing a function that adds all the numbers between 2 positive numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(posiNum1 , posiNum2) {
+    if (!Number.isInteger(posiNum1) || !Number.isInteger(posiNum2))
+        return "ERROR"
+    if (posiNum1 <0 || posiNum2 <0)
+        return "ERROR";
 
+    let sum = 0;
+    let max = Math.max(posiNum1,posiNum2);
+    let min = Math.min(posiNum1,posiNum2);
+    for (let i = min; i <= max; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
