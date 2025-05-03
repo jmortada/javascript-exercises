c0cd7fcc64eba20d4e6cf32bae1448da6701a95a
jmortada <158744391+jmortada@users.noreply.github.com>
2025-05-03 22:03:59 +0300

    finished calculater functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, curr) => sum + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, curr) => product * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(fac) {
+  let product = 1;
+  for (let i = fac; i > 1; i--) {
+    product *= i;
+  }
+  return product;
+	};
 
 // Do not edit below this line
 module.exports = {
