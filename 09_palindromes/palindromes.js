2a6d8ea935a48570940109f3ea67b35c42135785
jmortada <158744391+jmortada@users.noreply.github.com>
2025-05-04 11:40:31 +0300

    wrote a function to check if a string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    let cleanStr = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => {
+        let code = char.charCodeAt(0);
+        return (code >=48 && code <= 57) || (code >= 97 && code <= 122);
+    })
+    .join("");
+    let reversedStr = cleanStr.split("").reverse().join("");
+    
+    return cleanStr === reversedStr;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
