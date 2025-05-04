2369e9d96157e46a4da9a8e47244cc9a1f0bc832
jmortada <158744391+jmortada@users.noreply.github.com>
2025-05-04 16:44:38 +0300

    created a function to get the oldest person in an array of objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currYear = new Date().getFullYear();
+    let oldest = people.reduce((oldest, person) => {
+        let oldestAge = (oldest.yearOfDeath || currYear) - oldest.yearOfBirth;
+        let age = (person.yearOfDeath || currYear) - person.yearOfBirth;
+        if (age >oldestAge){
+            return person
+        }
+        return oldest
+    });
+    return oldest;
 };
 
 // Do not edit below this line
