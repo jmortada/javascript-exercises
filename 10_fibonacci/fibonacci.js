6e1d6ec16fa4e4763ec612ebfe3c642b290574f3
jmortada <158744391+jmortada@users.noreply.github.com>
2025-05-04 12:33:09 +0300

    created a function that returns a specific member of fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+    pos = Number(pos);
 
+    if (pos < 0) return 'OOPS';
+    if (pos === 0) return 0;
+    let fib = [1,1];
+    for (let i = 2; i < pos; i++) {
+        fib.push(fib[i-1] + fib[i-2]);
+
+    }
+    return fib[pos -1];
 };
 
 // Do not edit below this line
